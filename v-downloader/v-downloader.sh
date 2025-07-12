1d88a76ff6dcdf17c7f7d06cb045b9f3fd169747
chrv81 <vochris81@gmail.com>
2025-07-11 23:40:18 -0400

    Add directory prompt for saving files in v-downloader script

--- v-downloader/v-downloader.sh
+++ v-downloader/v-downloader.sh
@@ -123,8 +123,18 @@ main() {
     echo -e "${CYAN}Please enter the desired file title (without extension):${RESET}"
     title=$(prompt_user "text")
 
+    # Prompt for directory
+    while true; do
+      read -e -r -p "Enter the directory to save the file (absolute path): " save_dir
+      if [ -d "$save_dir" ]; then
+        break
+      else
+        echo -e "${RED}Directory does not exist. Please enter a valid directory.${RESET}"
+      fi
+    done
+
     # Run yt-dlp with user inputs
-    yt-dlp -o "${title}.%(ext)s" "$url"
+    yt-dlp -o "${save_dir}/${title}.%(ext)s" "$url"
 
   else
     echo -e "${YELLOW}yt-dlp is not installed.${RESET}"
